820708ba8ae2b321661cc492e2b1e16db92a9300
Robin Mayfield <robin@degu.io>
2021-02-27 17:02:21 +0000

    created layers and formatted

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ Key features:
 - No mouseover interaction on video
 - Interactive overlays
 - Walk/Drive options and instant updates
+
+## Resources
+
+- [w3schools - Fullscreen Video demo](https://www.w3schools.com/howto/howto_css_fullscreen_video.asp)
+- [React Player](https://www.npmjs.com/package/react-player)
+- [YouTube Player parameters](https://developers.google.com/youtube/player_parameters?playerVersion=HTML5#autoplay)
+- [React Icons](https://react-icons.github.io/react-icons/)
+
+Inspired by:
+
+- https://travel-remotely.netlify.app/
+- https://driveandlisten.herokuapp.com/
+- https://www.youtube.com/watch?v=9FEO-XKo4cw
+- https://twitter.com/fellowcreative
--- src/layers/VideoLayer.jsx
+++ src/layers/VideoLayer.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
+import { SiYoutube, SiYoutubestudio } from 'react-icons/si';
 import { videoData } from '../data';
 
 const VideoLayer = () => {
@@ -7,15 +8,20 @@ const VideoLayer = () => {
 
   return (
     <div className="container">
-      <div id="videoLayer">
-        <ReactPlayer url={currentVideo.url} />
-      </div>
+      <ReactPlayer id="video-layer" url={currentVideo.url} />
 
-      <div id="videoSelector">
+      <div id="video-selector">
         <div className="video-meta video-title">{currentVideo.title}</div>
         <div className="video-meta video-location">{currentVideo.location}</div>
         <div className="video-meta video-transport">
           {currentVideo.transport}
+        </div>
+        <div className="video-meta video-origin">
+          <SiYoutube className="icons" />{' '}
+          <a href={currentVideo.url}>Original Video</a>
+        </div>
+        <div className="video-meta video-creator">
+          <SiYoutubestudio className="icons" /> {currentVideo.creator}
         </div>
       </div>
     </div>
